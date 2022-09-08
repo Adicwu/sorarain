718a3694d1472104db498df232a9ff54fa4619eb
adicwu <youzaiwu@qq.com>
2022-09-08 19:10:34 +0800

    chore: 导出更新

--- packages/sorarain/src/index.ts
+++ packages/sorarain/src/index.ts
@@ -1,5 +1,6 @@
 export { default as AwVirtualWaterfall } from './VirtualWaterfall'
-export type { Requset as AwVirtualWaterfallRequset } from './VirtualWaterfall/type'
 export { default as AwDrag } from './Drag'
 export { default as AwListVueTransition } from './ListVueTransition'
 export { default as AwKeepAlives } from './KeepAlives'
+
+export * from './type'
---
+++ packages/sorarain/src/type.ts
@@ -0,0 +1,2 @@
+export type { Requset as AwVirtualWaterfallRequset } from './VirtualWaterfall/type'
+export type { AwDragExchangeParam } from './Drag/Drag'
