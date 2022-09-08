eea6187aea3a108da3ab8e697dc34bd670173a27
adicwu <youzaiwu@qq.com>
2022-09-08 15:23:40 +0800

    feat: 更新

--- packages/sorarain/src/index.ts
+++ packages/sorarain/src/index.ts
@@ -1,1 +1,5 @@
 export { default as AwVirtualWaterfall } from './VirtualWaterfall'
+export type { Requset as AwVirtualWaterfallRequset } from './VirtualWaterfall/type'
+export { default as AwDrag } from './Drag'
+export { default as AwListVueTransition } from './ListVueTransition'
+export { default as AwKeepAlives } from './KeepAlives'
